bdb66499b374f08cd194ff6f09829859593fe433
Cash Sullivan <Cashman7820@gmail.com>
2023-02-16 11:20:09 -0500

    added Dice program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //Random number gen
-            int max, min;
+            int max, min, dice1, dice2, randint1, randint2, randint3;
+            double randouble1, randouble2, randouble3;
             Console.WriteLine("Random Number Genarater");
             Console.WriteLine("Enter a minimum Value");
             min = int.Parse(Console.ReadLine());
@@ -19,6 +20,16 @@
             RandomNumberInRange = random.Next(min, max);
                 Console.WriteLine(" {0}, ", RandomNumberInRange);
             }
-        }
+            Console.WriteLine("");//------------------------------------------
+            Random myRandom = new Random();
+            //Generates Random Number
+            //Between 1 and 6\
+            dice1 = myRandom.Next(1, 6);
+            dice2 = myRandom.Next(1, 6);
+            //Results
+            Console.WriteLine($"You rolled {dice1} and {dice2}");
+            Console.WriteLine(""); //-----------------------------------------
+            
+        }   
     }
 }
